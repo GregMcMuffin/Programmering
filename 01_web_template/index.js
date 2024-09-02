5bcc1452d3d05d39c910fda4f6eee2304f3e025a
GregMcMuffin <Squirrelzhekai@gmail.com>
2024-09-02 22:34:03 +0200

    Finished page 2,3 and 4

--- 01_web_template/index.js
+++ 01_web_template/index.js
@@ -1,54 +1,66 @@
-let currentPage = 1
+let currentPage = 1;
 
-let pages //array med alle elementer med class = page 
-let menuItems //array med alle menupunkterne  
+let pages; // Array containing all elements with class = page
+let menuItems; // Array containing all menu items
 
-function setup(){
-    //shiftPage er funktionen der tager et tal og skifter til en side
+function setup() {
+    pages = selectAll('.page');
+    menuItems = selectAll('.menuitem');
 
-
-    pages = selectAll('.page')
-    menuItems = selectAll('.menuitem')
-
-    //menu items skal reagere ved at skifte side
-    for( m of menuItems ){
-        m.mousePressed( function(e) {
-            //e.target er selve html div'en 
-            console.log(e.target.id)
-            //slice -1 henter det sidste bogstav i en string
-            let nr = e.target.id.slice(-1)
-            //nu kan vi kalde shiftPage som skifter side
-            shiftPage(nr)
-        })
+    // Menu items should switch pages when clicked
+    for (let m of menuItems) {
+        m.mousePressed(function(e) {
+            let nr = e.target.id.slice(-1); // Get the last character of the id
+            shiftPage(nr); // Switch to the page
+        });
     }
-        
-    shiftPage(currentPage)
-    //vent to sekunder og sæt så klassen "hidden" på headeren - så menuen bliver væk
-    setTimeout(function(){
-        select('header').addClass('hidden')
-    }, 10000)
+    
+    shiftPage(currentPage);
+
+    // Hide the menu after 10 seconds
+    setTimeout(function() {
+        select('header').addClass('hidden');
+    }, 10000);
 }
 
-function shiftPage(num){
-    if(num == "ArrowLeft"){
-        num = currentPage - 1
+function shiftPage(num) {
+    if (num == "ArrowLeft") {
+        num = currentPage - 1;
     }
-    if(num == "ArrowRight"){
-        num = currentPage + 1
-    }
-
-    if(isNaN(num) || num > pages.length || num == 0){
-        return
+    if (num == "ArrowRight") {
+        num = currentPage + 1;
     }
 
-    select("#page" + currentPage).removeClass('visible')
-    select("#menu" + currentPage).removeClass('active')
-    currentPage = num
-    select("#page" + currentPage).addClass('visible')
-    select("#menu" + currentPage).addClass('active')
+    if (isNaN(num) || num > pages.length || num == 0) {
+        return;
+    }
+
+    select("#page" + currentPage).removeClass('visible');
+    select("#menu" + currentPage).removeClass('active');
+    currentPage = num;
+    select("#page" + currentPage).addClass('visible');
+    select("#menu" + currentPage).addClass('active');
+
+    // Automatically display image if page 3 is active
+    if (currentPage == 3) {
+        displayImage();
+    }
 }
 
-function keyPressed(){
-    console.log(key)
-    shiftPage(key)
+function displayImage() {
+    const imageUrl = "https://i.ibb.co/GPYK01Q/babadabooey.png"; // Fixed image URL
+
+    // Create an img element and set the src attribute to the URL
+    let imgElement = createImg(imageUrl, 'Uploaded image');
+    imgElement.style('max-width', '100%');
+    imgElement.style('height', 'auto');
+
+    // Append the image to the image container on page 3
+    let container = select('#imageContainer');
+    container.html(''); // Clear the container before adding a new image
+    container.child(imgElement);
+}
+
+function keyPressed() {
+    shiftPage(key);
 }
