03aca0e7e57398a952eeb419cadfb797802c3db7
GregMcMuffin <Squirrelzhekai@gmail.com>
2024-09-03 22:29:36 +0200

    Final changes

--- 01_web_template/index.js
+++ 01_web_template/index.js
@@ -7,20 +7,20 @@ function setup() {
     pages = selectAll('.page');
     menuItems = selectAll('.menuitem');
 
-    // Menu items should switch pages when clicked
+    // Menu items should switch pages when it reacts by the mousepressed function
     for (let m of menuItems) {
         m.mousePressed(function(e) {
-            let nr = e.target.id.slice(-1); // Get the last character of the id
+            let nr = e.target.id.slice(-1); // Get the last character in a string
             shiftPage(nr); // Switch to the page
         });
     }
     
     shiftPage(currentPage);
-
-    // Hide the menu after 10 seconds
+    //shiftPage is the function that takes a number and changes it to a page
     setTimeout(function() {
         select('header').addClass('hidden');
     }, 10000);
+    // Hide the menu after 10 seconds
 }
 
 function shiftPage(num) {
@@ -41,21 +41,21 @@ function shiftPage(num) {
     select("#page" + currentPage).addClass('visible');
     select("#menu" + currentPage).addClass('active');
 
-    // Automatically display image if page 3 is active
+    // Automatically displays the image if page 3 is the "active" page so to speak
     if (currentPage == 3) {
         displayImage();
     }
 }
 
 function displayImage() {
-    const imageUrl = "https://i.ibb.co/GPYK01Q/babadabooey.png"; // Fixed image URL
+    const imageUrl = "https://i.ibb.co/GPYK01Q/babadabooey.png"; // image URL
 
-    // Create an img element and set the src attribute to the URL
+    // I create an img element and set the src attribute to the URL
     let imgElement = createImg(imageUrl, 'Uploaded image');
     imgElement.style('max-width', '100%');
     imgElement.style('height', 'auto');
 
-    // Append the image to the image container on page 3
+    // Set the image to the image container on page 3
     let container = select('#imageContainer');
     container.html(''); // Clear the container before adding a new image
     container.child(imgElement);
